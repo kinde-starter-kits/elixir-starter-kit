72e9a6b9e66d2dc111e6a9522d4636533906e6e0
Hammadkhan <Hammadbd08@gmail.com>
2024-11-29 01:43:05 +0500

    Modified code for authentication

--- lib/demo_elixir_phoenix_web/plugs/check_auth.ex
+++ lib/demo_elixir_phoenix_web/plugs/check_auth.ex
@@ -1,5 +1,6 @@
 defmodule DemoElixirPhoenixWeb.Plugs.CheckAuth do
   import Plug.Conn
+  import Logger
   alias KindeClientSDK
 
   def init(default), do: default
@@ -7,14 +8,30 @@ defmodule DemoElixirPhoenixWeb.Plugs.CheckAuth do
   def call(conn, _opts) do
     access_token = get_session(conn, :access_token)
     refresh_token = get_session(conn, :refresh_token)
+    {conn, client} = KindeClientSDK.get_token(conn)
 
-    if access_token && refresh_token do
-      conn = KindeClientSDK.init_with_tokens(conn, access_token, refresh_token)
-      conn
-    else
-      conn
-      |> Phoenix.Controller.redirect(to: "/log-in")
-      |> halt()
+    authenticate(conn, client, access_token, refresh_token)
+  end
+
+  def authenticate(conn, client ,access_token, refresh_token) when is_binary(access_token) and is_binary(refresh_token) do
+    case client do
+      %{grant_type: :authorization_code_flow_pkce, auth_status: :authenticated} ->
+        conn
+      _ ->
+        Logger.warn("Unauthorized, redirecting to login")
+        redirect_to_login(conn)
     end
   end
+
+  def authenticate(conn, client, _, _) do
+    Logger.warn("Missing tokens, redirecting to login")
+    redirect_to_login(conn)
+  end
+
+  defp redirect_to_login(conn) do
+    conn
+    |> Phoenix.Controller.redirect(to: "/log-in")
+    |> halt()
+  end
+
 end
